e695afa325a37c2adeb850d7ca51ab78f53c6d2f
Mikhail <mcmishabest@gmail.com>
2021-11-08 19:32:55 +0600

    Initial commit

---
+++ src/main/java/NewsPageRestAPI/NewsPageRestApiApplication.java
@@ -0,0 +1,13 @@
+package NewsPageRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsPageRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsPageRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NewsPageRestAPI/NewsPageRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package NewsPageRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsPageRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
